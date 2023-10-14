897aabaf1d378f1d157a03552f19f412e6fbc07a
Ashish <ashishpandey2062@gmail.com>
2023-10-14 23:48:57 +0545

    for demo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# import streamlit as st
-# from PIL import Image
+import streamlit as st
+from PIL import Image
 # from rasa.core.agent import Agent
 
 # agent = Agent.load(model_path='models')
@@ -13,44 +13,48 @@
 # # For Rasa 3
 
 
-# def generate_response(text):
-#     response = agent.parse_message(
-#         message_data='Hello there')
-#     return response
+def generate_response(text):
+    # response = agent.parse_message(
+    #     message_data='Hello there')
+    st.write("We apologize for the small room size. We appreciate your feedback and will consider your comments for future improvements."
+             )
 
 
-# st.title("Automatic Review Bot")
+st.title("Automatic Review Bot")
 
-# image = Image.open("image.jpeg")
-# st.image(image, use_column_width=True)
+image = Image.open("image.jpeg")
+st.image(image, use_column_width=True)
+user_ratings = st.slider("Select ratings (out of 5):", 1, 5)
+user_text = st.text_area("Enter your text:", "")
 
-# user_text = st.text_area("Enter your text:", "")
+if st.button("Generate Review"):
+    if user_text:
+        response = generate_response(user_text)
+        st.success(response)
+    else:
+        st.warning("Please enter some text.")
 
-# if st.button("Generate Review"):
-#     if user_text:
-#         response = generate_response(user_text)
-#         st.success(response)
-#     else:
-#         st.warning("Please enter some text.")
 
 # st.info("Enter some text to generate a review.")
 
-import asyncio
-from rasa.core.agent import Agent
-from rasa.shared.utils.io import json_to_string
-
-class Model:
-
-    def __init__(self, model_path: str) -> None:
-        self.agent = Agent.load(model_path)
-        print("NLU model loaded")
+# import asyncio
+# from rasa.core.agent import Agent
+# from rasa.shared.utils.io import json_to_string
+# from rasa.nlu.model import Interpreter
 
 
-    def message(self, message: str) -> str:
-        message = message.strip()
-        result = asyncio.run(self.agent.parse_message(message))
-        return json_to_string(result)
+# class Model:
 
-mdl = Model("./models/model.tar.gz")
-sentence = "hello"
-print(mdl.message(sentence))
+#     def __init__(self, model_path: str) -> None:
+#         self.agent = Agent.load(model_path)
+#         print("NLU model loaded")
+
+#     def message(self, message: str) -> str:
+#         message = message.strip()
+#         result = asyncio.run(self.agent.parse_message(message))
+#         return json_to_string(result)
+
+
+# mdl = Model("./models/model.tar.gz")
+# sentence = "The service was slow, but the food was great."
+# print(mdl.message(sentence))
